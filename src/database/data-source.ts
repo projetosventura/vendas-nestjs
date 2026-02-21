f3daf01ba1069d83a5495fc61e807ba085549dac
Vinicius Ventura <projetosventura@gmail.com>
2026-02-21 16:30:49 -0300

    FIX: variables in data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost', // ou o nome do container se estiver via docker-compose
-  port: 5432,
-  username: 'nestuser',
-  password: 'nestpass',
-  database: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migration/*.ts'],
   synchronize: false,
