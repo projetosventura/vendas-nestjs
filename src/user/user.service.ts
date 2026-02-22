f87d7162dc140bf2e4cb5c1e532f59381c0e1a9b
Vinicius Ventura <projetosventura@gmail.com>
2026-02-21 21:11:33 -0300

    revert

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { hash } from 'bcrypt';
 import { createUserDto } from './dtos/create-user.dto';
 import { UserEntity } from './interfaces/user.entity';
@@ -11,55 +11,22 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: createUserDto): Promise<UserEntity> {
-    try {
-      const passwordHashed = await hash(
-        createUserDto.password,
-        10,
-      );
+    const passwordHashed = await hash(
+      createUserDto.password,
+      10,
+    );
 
-      return await this.userRepository.create({
-        ...createUserDto,
-        password: passwordHashed,
-      });
-    } catch (error) {
-      
-      if (error.code === '23505') {
-        throw new ConflictException('Email já cadastrado');
-      }
-
-      throw new InternalServerErrorException(
-        'Erro ao criar usuário',
-      );
-    }
+    return this.userRepository.create({
+      ...createUserDto,
+      password: passwordHashed,
+    });
   }
 
   async getAllUsers(): Promise<UserEntity[]> {
-    try {
-      return await this.userRepository.findAll();
-    } catch (error) {
-      throw new InternalServerErrorException(
-        'Erro ao buscar usuários',
-      );
-    }
+    return this.userRepository.findAll();
   }
 
   async findByEmail(email: string): Promise<UserEntity[]> {
-    try {
-      const user = await this.userRepository.findByEmail(email);
-
-      if (!user) {
-        throw new NotFoundException('Usuário não encontrado');
-      }
-
-      return user;
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-
-      throw new InternalServerErrorException(
-        'Erro ao buscar usuário',
-      );
-    }
+    return this.userRepository.findByEmail(email);
   }
 }
